e6d6cde286be1fc08e53777d5fe73f08198e604d
Hannes | hannesgao.eth <hannes.gao@gmail.com>
2024-11-28 17:26:52 +0100

    Comments updated

--- script/DeployHannesExchangeV1Pair.s.sol
+++ script/DeployHannesExchangeV1Pair.s.sol
@@ -10,6 +10,9 @@ import "forge-std/Script.sol";
 /// 部署前需在 .env 以及 foundry.toml 中设置部署用测试EOA账户私钥 PRIVATE_KEY_1 和EtherScan的API秘钥 ETHERSCAN_API_KEY
 /// 部署前请一定确认 .gitignore 中有 .env 文件，以确保私钥和API秘钥不被同步至远程仓库
 
+/// 注意：
+/// 本部署脚本仅做测试之用，流动性池合约的部署原则上需要由工厂合约来管理
+
 /// 部署命令：
 /// forge script script/DeployHannesExchangeV1Pair.s.sol --rpc-url sepolia --broadcast --verify
 
@@ -17,7 +20,7 @@ import "forge-std/Script.sol";
 /// HannesExchangeV1Factory 逻辑合约地址：0x4861517289ed9b9a837cAD1433c41DC3C5eF7494
 /// HannesExchangeV1Factory 代理合约地址：0xCE93b8542a1F6fF1A60C22Ed4666118512faD696
 
-/// 部署流动性池所需的，已部署好的ERC20代币合约地址：
+/// 部署流动性池所需的，已部署好的，可用于测试用的ERC20代币合约地址：
 /// Hannes Exchange Simple Token (HEST) 合约地址：0xC6A8d2850F326B769bb4e3187621d2B5f18dBFd3
 /// Hannes Exchange Test TokenV2 (HETTV2) 代理合约地址：0xb711c0fA82D2ed7814282c83AE15B6F944DD2b50
 
@@ -32,7 +35,7 @@ contract DeployHannesExchangeV1Pair is Script {
         address factoryProxy = 0xCE93b8542a1F6fF1A60C22Ed4666118512faD696;
         
         /// 指定要创建流动性池的ERC20代币地址
-        address tokenAddress = vm.envAddress("TOKEN_ADDRESS");
+        address tokenAddress = vm.envAddress("TOKEN_ADDRESS");  /// 占位符，请替换为实际的ERC20代币地址
         
         /// 设置各个访问控制角色的地址，为简化测试部署，皆设置为 TEST_EOA_ADDRESS_1
         /// 可根据部署和访问控制需求修改为不同的EOA账户
